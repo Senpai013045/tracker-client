b609150ded10e1eaaad5c8f8ff7cdf0a0e9ed32a
Senpai013045 <subham.hero01@gmail.com>
2022-05-01 23:05:09 +0545

    simple sse tested

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("hello world lol")
+import express from "express";
+
+const app = express();
+app.use(express.static("public"));
+
+//a SSE route
+app.get("/sse", (req, res) => {
+    res.writeHead(200, {
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        Connection: "keep-alive"
+    });
+    res.write("data: The Server is working\n\n");
+    setInterval(() => {
+        res.write("data: The Server is working\n\n");
+    }, 1000);
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+})
